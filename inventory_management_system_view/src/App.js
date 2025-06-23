ce01f36ed01f3574f073680a7e2eea2db7ded825
NehemiahPietersen <nehemiah11pietersen@gmail.com>
2025-06-23 16:23:01 +0200

    implemented wokring login page

--- inventory_management_system_view/src/App.js
+++ inventory_management_system_view/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/register" element={<RegisterPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
       </Routes>
     </Router>
   );
